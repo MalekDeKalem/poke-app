b6356028f3d4832b8db0fb48e85232460889e513
dorian <dimitri200169@gmail.com>
2024-12-13 11:11:59 +0100

    added fontawesome

--- src/lib/components/Pagination.svelte
+++ src/lib/components/Pagination.svelte
@@ -26,6 +26,12 @@
 
 <div class="justify-center flex">
     <div class="bg-gradient-to-r from-slate-600 to-slate-400 px-0.5 justify-center flex py-0.5 rounded">
+        <div class="justify-center flex items-center">
+            <!-- svelte-ignore a11y_consider_explicit_label -->
+            <a href="/?limit={pageSize}&offset={pageSize * (currentPage-1)}">
+                <i class="fa-solid fa-arrow-left fa-2x"></i>
+            </a>
+        </div>
         {#each getPageRange(currentPage) as pageNum}
             {#if pageNum === currentPage}
                 <div class="rounded-lg bg-gray-800 size-11 mx-0.5">
@@ -33,11 +39,26 @@
                 </div>
             {:else}
                 <div class="rounded-lg bg-gray-800 size-11 mx-0.5">
-                    <a class="text-rose-600 justify-center flex items-center h-full" href="/?limit={pageSize}&offset={pageSize * pageNum}"> {pageNum + 1}</a>
+                    <a class="text-rose-600 justify-center flex items-center h-full hover:text-lime-600" href="/?limit={pageSize}&offset={pageSize * pageNum}"> {pageNum + 1}</a>
                 </div>
             {/if}
         {/each}
+        <div class="justify-center flex items-center">
+            <!-- svelte-ignore a11y_consider_explicit_label -->
+            <a href="/?limit={pageSize}&offset={pageSize * (currentPage+1)}">
+                <i class="fa-solid fa-arrow-right fa-2x"></i>
+            </a>
+        </div>
     </div>
 </div>
 
 
+<style>
+    .fa-arrow-right:hover {
+        @apply hover:text-lime-600;
+    } 
+
+    .fa-arrow-left:hover {
+        @apply hover:text-lime-600;
+    } 
+</style>
