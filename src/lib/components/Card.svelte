167325b496200974ed39620f2880fc26efdfadbe
dorian <dimitri200169@gmail.com>
2025-01-10 21:34:48 +0100

    svg improved

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -2,12 +2,20 @@
     import PokeBallIcon from "$lib/icons/PokeBallIcon.svelte";
 
     let { image="s", name } = $props();
+
+
+    const rand = (min: number, max: number) => {
+        const minCeiled = Math.ceil(min);
+        const maxFloored = Math.floor(max);
+        return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+    }
+
 </script>
 
 <div class="rounded-lg justify-center items-center bg-gray-600">
     <div class="m-1 p-px bg-gray-800 rounded-lg">
         <div class="rounded-lg">
-            <PokeBallIcon />
+            <PokeBallIcon rotInnerCircle={rand(0, 360)}/>
         </div>
     </div>
     <div>
