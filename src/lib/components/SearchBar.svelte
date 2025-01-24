e9c63d46d8a48a325cf89f5a54d57441935c2964
dorian <dimitri200169@gmail.com>
2025-01-25 00:27:02 +0100

    search bar

---
+++ src/lib/components/SearchBar.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    let query = $state<string>("");
+</script>
+
+
+
+<div>
+    <input type="text" placeholder="search" bind:value={query} class="rounded-lg h-9" />
+</div>
--- src/lib/components/TopBar.svelte
+++ src/lib/components/TopBar.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
+    import SearchBar from "./SearchBar.svelte";
+
 
 </script>
 
 
 
 <div class="w-full h-20 mb-2 bg-red-200 grid grid-cols-3">
-<div></div>
-<h1 class="flex justify-center"> Pokemon </h1>
-
+    <h1 class="flex justify-center items-center"> Pokemon </h1>
+    <div class="flex justify-center items-center">
+        <SearchBar />
+    </div>
 </div>
