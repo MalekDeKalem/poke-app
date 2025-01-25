98bff27b72a4a7cc407d846fbec3506129038c3d
dorian <dimitri200169@gmail.com>
2025-01-25 14:22:27 +0100

    shit

--- src/lib/components/SearchBar.svelte
+++ src/lib/components/SearchBar.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
+    import { pokeStore } from "../../routes/+page.svelte"
     let query = $state<string>("");
+
+        
+    const handleInput = (event: any) => {
+    
+    }
 </script>
 
 
 
 <div>
-    <input type="text" placeholder="search" bind:value={query} class="rounded-lg h-9" />
+    <input on:change={handleInput} type="text" placeholder="search" bind:value={query} class="rounded-lg h-9" />
 </div>
