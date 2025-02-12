67c9e90c7c6ecad46eae3fefcb58b713343f1507
dorian <dimitri200169@gmail.com>
2025-02-12 16:23:49 +0100

    canvas reactive

--- src/lib/components/PokemonCard.svelte
+++ src/lib/components/PokemonCard.svelte
@@ -5,50 +5,67 @@
     let el = $state<HTMLCanvasElement>();
 
     onMount(() => {
-		let camera : THREE.PerspectiveCamera;
-		let scene : THREE.Scene;
-		let renderer : THREE.WebGLRenderer;
-		let cube : THREE.Mesh<THREE.BoxGeometry, THREE.MeshBasicMaterial>;
+		if (!el) {
+			console.error("Canvas not found");
+			return;
+    	}
+
+		let camera: THREE.PerspectiveCamera;
+		let scene: THREE.Scene;
+		let renderer: THREE.WebGLRenderer;
+		let cube: THREE.Mesh;
 
 		const init = () => {
 			scene = new THREE.Scene();
-			camera = new THREE.PerspectiveCamera( 75, window.innerWidth / (window.innerHeight * 1.5), 0.1, 1000 );
-			const canvas = document.getElementById("canvas");
-			renderer = new THREE.WebGLRenderer({antialias: true, canvas: el});
-			renderer.setSize(canvas?.clientWidth! * 0.8, canvas?.clientHeight! * 1.5);
-			document.body.appendChild( renderer.domElement );
+			camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-			console.log(canvas?.clientHeight);
-
-			const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-			const material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
-			cube = new THREE.Mesh( geometry, material );
-			scene.add( cube );
+			renderer = new THREE.WebGLRenderer({ antialias: true, canvas: el });
+			renderer.setSize(window.innerWidth, window.innerHeight);
+			renderer.setPixelRatio(window.devicePixelRatio);
+			
+			// Cube setup
+			const geometry = new THREE.BoxGeometry(1, 1, 1);
+			const material = new THREE.MeshBasicMaterial({ color: 0x00ff88 });
+			cube = new THREE.Mesh(geometry, material);
+			scene.add(cube);
 
 			camera.position.z = 5;
-		}
+		};
 
 		const render = () => {
-			renderer.clear()
 			renderer.render(scene, camera);
-		}
+		};
 
 		const animate = () => {
-			requestAnimationFrame(animate)
+			requestAnimationFrame(animate);
 
-			cube.rotation.x += 0.005
-			cube.rotation.y += 0.005
+			cube.rotation.x += 0.005;
+			cube.rotation.y += 0.05;
+			cube.rotation.z += 0.1;
+			camera.rotation.x += 0.05;
 
-			render()
-		}
+			render();
+		};
 
-		init()
-		animate()
+		const resizeCanvas = () => {
+			if (!el) return;
+
+			const width = window.innerWidth;
+			const height = window.innerHeight;
+			renderer.setSize(width, height);
+			camera.aspect = width / height;
+			camera.updateProjectionMatrix();
+		};
+
+		init();
+		animate();
+		window.addEventListener("resize", resizeCanvas);
     });
 
 
 </script>
 
-<canvas class="h-[500px] relative top-0 bottom-0 left-0 right-0 m-auto" id="canvas" bind:this={el}>
+<canvas class="laptop:h-[500px] relative top-0 bottom-0 left-0 right-0 m-auto" id="canvas" bind:this={el}>
 
 </canvas>
+
