e1d17e0c61cbc59704b3d0655f626d4500707b07
MalekDeKalem <dimitri200169@gmail.com>
2026-02-24 01:21:36 +0100

    added card model with texture

--- src/lib/components/PokemonCard.svelte
+++ src/lib/components/PokemonCard.svelte
@@ -1,78 +1,94 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-	import * as THREE from 'three';
+  import { onMount } from "svelte";
+  import * as THREE from "three";
+  import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
-    let el = $state<HTMLCanvasElement>();
+  let el = $state<HTMLCanvasElement>();
+  let { textureImage } = $props();
 
-    onMount(() => {
-		if (!el) {
-			console.error("Canvas not found");
-			return;
-    	}
+  onMount(() => {
+    if (!el) {
+      console.error("Canvas not found");
+      return;
+    }
 
-		let camera: THREE.PerspectiveCamera;
-		let scene: THREE.Scene;
-		let renderer: THREE.WebGLRenderer;
-		let cube: THREE.Mesh;
+    let camera: THREE.PerspectiveCamera;
+    let scene: THREE.Scene;
+    let renderer: THREE.WebGLRenderer;
 
-		const init = () => {
-			scene = new THREE.Scene();
-      const width = el.clientWidth;
-      const height = el.clientHeight;
-			camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+    const init = async () => {
+      scene = new THREE.Scene();
+      const width = el?.clientWidth;
+      const height = el?.clientHeight;
+      camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
 
-			renderer = new THREE.WebGLRenderer({ antialias: true, canvas: el });
-			renderer.setSize(width, height, false);
-			renderer.setPixelRatio(window.devicePixelRatio);
-			
-			// Cube setup
-			const geometry = new THREE.BoxGeometry(1, 1, 1);
-			const material = new THREE.MeshBasicMaterial({ color: 0x00ff88 });
-			cube = new THREE.Mesh(geometry, material);
-			scene.add(cube);
+      renderer = new THREE.WebGLRenderer({ antialias: true, canvas: el });
+      renderer.setSize(width, height, false);
+      renderer.setPixelRatio(window.devicePixelRatio);
+      const light = new THREE.AmbientLight(0xffffff);
+      scene.add(light);
 
-			camera.position.z = 5;
-		};
+      const textureLoader = new THREE.TextureLoader();
+      const texture = await textureLoader.loadAsync(textureImage);
+      texture.center.set(0.5, 0.5);
+      texture.rotation = Math.PI / 2;
 
-		const render = () => {
-			renderer.render(scene, camera);
-		};
+      texture.colorSpace = THREE.SRGBColorSpace;
 
-		const animate = () => {
-			requestAnimationFrame(animate);
+      const loader = new GLTFLoader();
+      const card = await loader.loadAsync("/models/PokeCard.glb");
+
+      card.scene.traverse((child) => {
+        if (child instanceof THREE.Mesh && child.name === "Plane_2") {
+          console.log("Mesh:", child.name);
+          child.material = new THREE.MeshBasicMaterial({
+            map: texture,
+            transparent: true,
+          });
+        }
+      });
+
+      scene.add(card.scene);
+
+      camera.position.x = 2;
+      camera.position.y = 1.5;
+      camera.rotation.y = Math.PI / 2;
+    };
+
+    const render = () => {
+      renderer.render(scene, camera);
+    };
+
+    const animate = () => {
+      requestAnimationFrame(animate);
 
       resizeCanvas();
 
-			cube.rotation.x += 0.005;
-			cube.rotation.y += 0.05;
-			cube.rotation.z += 0.1;
-			camera.rotation.x += 0.05;
+      render();
+    };
 
-			render();
-		};
+    const resizeCanvas = () => {
+      if (!el) return;
 
-		const resizeCanvas = () => {
-			if (!el) return;
-
-			const width = el.clientWidth;
-			const height = el.clientHeight;
+      const width = el.clientWidth;
+      const height = el.clientHeight;
 
       if (el.width !== width || el.height !== height) {
         renderer.setSize(width, height, false);
         camera.aspect = width / height;
         camera.updateProjectionMatrix();
       }
-		};
+    };
 
-		init();
-		animate();
-		window.addEventListener("resize", resizeCanvas);
-    });
-
-
+    init();
+    animate();
+    window.addEventListener("resize", resizeCanvas);
+  });
 </script>
 
-<canvas class="laptop:h-[500px] max-w-full h-auto bg-white border" id="canvas" bind:this={el}>
-
+<canvas
+  class="laptop:h-[500px] max-w-full h-auto bg-white border"
+  id="canvas"
+  bind:this={el}
+>
 </canvas>
-
--- src/routes/pokemon/[slug]/+page.svelte
+++ src/routes/pokemon/[slug]/+page.svelte
@@ -1,31 +1,37 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-    import { page } from '$app/stores';
-    import { loadPokemon } from '$lib/api';
-    import PokemonCard from '$lib/components/PokemonCard.svelte';
+  import { onMount } from "svelte";
+  import { page } from "$app/stores";
+  import { loadPokemon } from "$lib/api";
+  import PokemonCard from "$lib/components/PokemonCard.svelte";
 
-    let { data } = $props()
+  let { data } = $props();
 </script>
 
-
 <div>
-    <h1 class="flex justify-center font-bold text-2xl"> {data.pokemon.name.toUpperCase()} </h1>
-    <hr class="rounded-lg border-2 mx-9">
-    <img alt="picture of pokemon {data.pokemon.name}" src="{data.pokemon.sprites.front_default}"/>
-    <p> His height is {data.pokemon.height}</p>
-    <p>Experience: {data.pokemon.base_experience}</p>
-    <figure>
-        <figcaption> Listen to the Cry of {data.pokemon.name} </figcaption>
-        {#if data.pokemon.cries.latest}
-            <audio class="rounded-lg" controls src={data.pokemon.cries.latest}></audio>
-        {:else}
-            <audio class="rounded-lg" controls src={data.pokemon.cries.legacy}></audio>
-        {/if}
-    </figure>
-   
-    <div class="grid place-items-center">
-      <div class="w-1/2 border-4 border-red-400 grid place-items-center">
-        <PokemonCard />
-      </div>
+  <h1 class="flex justify-center font-bold text-2xl">
+    {data.pokemon.name.toUpperCase()}
+  </h1>
+  <hr class="rounded-lg border-2 mx-9" />
+  <img
+    alt="picture of pokemon {data.pokemon.name}"
+    src={data.pokemon.sprites.front_default}
+  />
+  <p>His height is {data.pokemon.height}</p>
+  <p>Experience: {data.pokemon.base_experience}</p>
+  <figure>
+    <figcaption>Listen to the Cry of {data.pokemon.name}</figcaption>
+    {#if data.pokemon.cries.latest}
+      <audio class="rounded-lg" controls src={data.pokemon.cries.latest}
+      ></audio>
+    {:else}
+      <audio class="rounded-lg" controls src={data.pokemon.cries.legacy}
+      ></audio>
+    {/if}
+  </figure>
+
+  <div class="grid place-items-center">
+    <div class="w-1/2 border-4 border-red-400 grid place-items-center">
+      <PokemonCard textureImage={data.pokemon.sprites.front_default} />
     </div>
+  </div>
 </div>
